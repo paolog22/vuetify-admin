42fe299b7d419e0bb192e1d67616431263fb70b0
Marcelo Barros da Silva <marcelo.bsilva@outlook.com.br>
2017-12-06 01:51:25 -0200

    pages8

--- build/pages.js
+++ build/pages.js
@@ -11,7 +11,7 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.pages.conf')
 
-const spinner = ora('building for production...')
+const spinner = ora('building for GitHub Pages...')
 spinner.start()
 
 rm(path.join(config.pages.assetsRoot, config.pages.assetsSubDirectory), err => {
